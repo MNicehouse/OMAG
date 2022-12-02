89fb9a52f6b412f1345a39d01109cb5e872fcc13
Alexander Belski <sascha.belski@gmail.com>
2022-12-02 10:43:00 +0100

    Code added: navigation through questions

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -16,22 +16,26 @@ class ResponsesController < ApplicationController
       @questions.map { |u| quest_arr << u.id }
       quest_arr.sort
       @myquestionid = params[:myassessquestionid].nil? ? -1 : params[:myassessquestionid].to_i
+      @dir_param = params[:dir_param].nil? ? 'forth' : params[:dir_param]
       # forth
-      @myquestionindex_new = quest_arr.find_index(@myquestionid).nil? ? 0 : quest_arr.find_index(@myquestionid) + 1
-      if (@myquestionindex_new) >= quest_arr.count
-        render "users/show", status: :unprocessable_entity
-      else
-        @myquestionid_new = quest_arr[@myquestionindex_new]
+      if @dir_param == "forth"
+        @myquestionindex_new = quest_arr.find_index(@myquestionid).nil? ? 0 : quest_arr.find_index(@myquestionid) + 1
+        if (@myquestionindex_new) >= quest_arr.count
+          render "users/show"
+        else
+          @myquestionid_new = quest_arr[@myquestionindex_new]
+        end
       end
       # back
-      @myquestionindex_new_pre = quest_arr.find_index(@myquestionid).nil? ? 0 : quest_arr.find_index(@myquestionid) - 1
-      if (@myquestionindex_new_pre) < 0
-        @myquestionindex_new_pre = 0
-        @myquestionid_new_pre = @myquestionid
-      else
-        @myquestionid_new_pre = quest_arr[@myquestionindex_new_pre]
+      if @dir_param == "back"
+        @myquestionindex_new = quest_arr.find_index(@myquestionid).nil? ? 0 : quest_arr.find_index(@myquestionid) - 1
+        if (@myquestionindex_new) < 0
+          @myquestionindex_new = 0
+          @myquestionid_new = @myquestionid
+        else
+          @myquestionid_new = quest_arr[@myquestionindex_new]
+        end
       end
-      @dir_param = params[:dir_param].nil? ? 'forth' : params[:dir_param]
     end
 
     private
