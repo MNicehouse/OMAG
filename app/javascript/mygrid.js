b59e57e5f69d72f8f3ad8407a0125ba5761aa6f1
Alexander Belski <sascha.belski@gmail.com>
2022-12-08 11:03:16 +0100

    last touches

--- app/javascript/mygrid.js
+++ app/javascript/mygrid.js
@@ -10,8 +10,8 @@ document.querySelectorAll('tr.assessment-tr-mygrid').forEach(function(el){
         this.closest('tr').classList.remove("tr-clicked");
         // add highlight to the parent tr of the clicked td
         this.closest('tr').classList.add("tr-clicked");
-        document.getElementById("assessment_id").value = this.closest('tr').cells[0].textContent;
-        document.getElementById("assessment_name").value = this.closest('tr').cells[2].textContent;
+        if (document.getElementById("assessment_id") != null) { document.getElementById("assessment_id").value = this.closest('tr').cells[0].textContent; }
+        if (document.getElementById("assessment_name") != null) { document.getElementById("assessment_name").value = this.closest('tr').cells[2].textContent; }
     });
   });
   //new user
@@ -25,7 +25,7 @@ document.querySelectorAll('tr.assessment-tr-mygrid').forEach(function(el){
       this.closest('tr').classList.remove("tr-clicked");
       // add highlight to the parent tr of the clicked td
       this.closest('tr').classList.add("tr-clicked");
-      document.getElementById("user_id").value = this.closest('tr').cells[0].textContent;
-      document.getElementById("user_name").value = this.closest('tr').cells[2].textContent;
+      if (document.getElementById("user_id") != null) { document.getElementById("user_id").value = this.closest('tr').cells[0].textContent; }
+      if (document.getElementById("user_name") != null) { document.getElementById("user_name").value = this.closest('tr').cells[2].textContent; }
     });
   });
