718b4b0ac2f77f0a2f98f2c1290f54c4c32ae342
Alexander Belski <sascha.belski@gmail.com>
2022-12-08 16:26:17 +0100

    the-last-branch

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,5 +1,6 @@
 class AssessmentsController < ApplicationController
   before_action :authenticate_admin!, only: [ :new, :create ]
+  # rescue_from Pagy::OverflowError, with: :redirect_to_last_page
 
   def new
     @assessment = Assessment.new
@@ -32,9 +33,9 @@ class AssessmentsController < ApplicationController
   def index
     # @assessments = Assessment.all
     @redir = params[:redir_param].nil? ? 0 : params[:redir_param].to_i
-    @pagy, @assessments = pagy(Assessment.all, items: 10)
-    @pagy_drafts, @assessments_drafts = pagy(Assessment.where(state: false).nil? ? [] : Assessment.where(state: false).reorder(:created_at), items: 10)
-    @pagy_ready, @assessments_ready = pagy(Assessment.where(state: true).nil? ? [] : Assessment.where(state: true).reorder(:created_at), items: 10)
+    @assessments = Assessment.all
+    @pagy_drafts, @assessments_drafts = pagy(Assessment.where(state: false).nil? ? [] : Assessment.where(state: false).reorder(:created_at), items: 20)
+    @pagy_ready, @assessments_ready = pagy(Assessment.where(state: true).nil? ? [] : Assessment.where(state: true).reorder(:created_at), items: 20)
     @role = role(current_user)
     @assessnotchosen = true
   end
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,15 @@
+require 'pagy/extras/bootstrap'
+require 'pagy/extras/overflow'
+
+# default :empty_page (other options :last_page and :exception )
+Pagy::DEFAULT[:overflow] = :last_page
+
+# OR
+require 'pagy/countless'
+require 'pagy/extras/overflow'
+
+# default :empty_page (other option :exception )
+Pagy::DEFAULT[:overflow] = :exception
 # frozen_string_literal: true
 
 # Pagy initializer file (5.10.1)
